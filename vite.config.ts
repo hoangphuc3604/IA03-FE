169acb7b5c2c030669fde11c30a8309477010d15
22120278 <nvhphuc364@gmail.com>
2025-10-26 21:05:45 +0700

    Add preview configuration in vite.config.ts to enable external access on port 4173

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['ia03-fe.onrender.com']
+  }
 })
